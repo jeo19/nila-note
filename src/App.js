12235967fa7a5d771d490cc245d9030798efd64d
jeo19 <terrakkk@outlook.com>
2018-10-23 00:04:11 +0630

    Add Routes

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-class App extends Component {
-  render() {
-    return <div className="App">gggg</div>;
-  }
-}
-
-export default App;
